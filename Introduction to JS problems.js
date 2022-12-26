7c47a96007004ab98dfdd33b958af5898e0e5f1a
sunil8590 <nani859095@gmail.com>
2022-12-26 17:46:23 +0530

    comment

---
+++ Introduction to JS problems.js
@@ -0,0 +1,49 @@
+let name = "Masai School";
+let aim = "A Transformation in Education";
+
+console.log(name);
+console.log(aim);
+
+
+
+//----------------------------------------------------
+
+let name1 = "Sunil Kumar";
+
+console.log(name1);
+
+name1 = "Satya Narayana";
+
+console.log(name1);
+
+name1 = "Mythri";
+
+console.log(name1)
+
+//---------------------------------------------------
+
+let name2 = "Sunil kumar";
+let age = 22;
+
+console.log(name2);
+console.log(typeof (name2));
+console.log(age);
+console.log(typeof (age));
+
+//-----------------------------------------------------------
+
+console.log(`                           ℝ𝕖𝕡𝕠𝕣𝕥ℂ𝕒𝕣𝕕
+                          🄼🄰🅂🄰🄸 🅂🄲🄷🄾🄾🄻
+          ****************************************                
+          |  Name : 🅢🅤🅝🅘🅛 🅚🅤🅜🅐🅡 🅝
+          |  Grade : 5th
+          |  Section : D
+          |  Roll no : 302
+          |  ---------------------------------------
+          |  Javascript       |        𝟡𝟘
+          |  Java             |        𝟡𝟚
+          |  DSA              |        𝟡𝟘
+                                ____________________
+          | 🅃🄾🅃🄰🄻                     1̲8̲2̲
+          *****************************************
+            `)
