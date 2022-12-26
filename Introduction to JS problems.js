f3cfb6a65a872037d0cf6309b1b06a1c99bf5bd2
sunil kumar <115404992+Sunil8590@users.noreply.github.com>
2022-12-26 18:38:23 +0530

    problem 3 updated from browser itself

--- Introduction to JS problems.js
+++ Introduction to JS problems.js
@@ -61,3 +61,7 @@ function multiplyBy50(number) {
 
 
 //--------------------------------------------------------------
+function productOfSix(one, two, three, four, five, six) {
+    // Write code here
+    console.log(one*two*three*four*five*six)
+}
